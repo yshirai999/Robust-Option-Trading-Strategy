fd7892cb4723574b8cea9c376799b0082c893824
yshirai999 <yshirai@uw.edu>
2024-06-03 15:25:54 -0700

    v

---
+++ BGDataReader.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.io import loadmat
+import pandas as pd
+import matlab.engine
+eng = matlab.engine.start_matlab()
+eng.BGDataReader(nargout = 0)
+mat = loadmat('bgset512.mat')
